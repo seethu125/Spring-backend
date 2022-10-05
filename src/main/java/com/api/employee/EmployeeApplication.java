31a10ed4606a6508c2b1c455e36298d50eaf0173
Seethu P B <seethubhaskaran@gmail.com>
2022-10-05 22:22:54 +0530

    Product added

--- src/main/java/com/api/employee/EmployeeApplication.java
+++ src/main/java/com/api/employee/EmployeeApplication.java
@@ -2,10 +2,6 @@ package com.api.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeApplication {
--- src/main/java/com/api/employee/controllers/ProductController.java
+++ src/main/java/com/api/employee/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.api.employee.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.api.employee.entities.ProductEntity;
 import com.api.employee.repos.ProductRepository;
 
 @RestController
+@CrossOrigin
 public class ProductController {
 	
 	@Autowired
